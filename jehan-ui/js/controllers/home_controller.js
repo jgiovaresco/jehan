a2e6638a35b0ee121642b698d36a26a893439101
jgiovaresco <dev@giovaresco.fr>
2014-06-01 14:49:23 +0200

    On home page, show only instances with jobs KO and show only these jobs

--- jehan-ui/js/controllers/home_controller.js
+++ jehan-ui/js/controllers/home_controller.js
@@ -1,6 +1,22 @@
 jehanControllers.controller('HomeCtrl', function ($scope, JehanService) {
+
+    /**
+     * @ngdoc property
+     * @module HomeCtrl
+     * @returns {Array.<Object>} List of jobs grouped by the name of the instance.
+     * @description
+     * Holds the list of jobs grouped by the name of the instance.
+     */
     $scope.jobs = [];
-    $scope.instances = JehanService.instances.query(
+
+    /**
+     * @ngdoc property
+     * @module HomeCtrl
+     * @returns {Array.<Object>} List of Jenkins instances.
+     * @description
+     * Holds the list of Jenkins instances.
+     */
+    $scope.instances = JehanService.instances.query({filter: "jobsKO"},
         function () {
             angular.forEach($scope.instances, function (p_instance)
             {
@@ -8,8 +24,35 @@ jehanControllers.controller('HomeCtrl', function ($scope, JehanService) {
             });
         });
 
+    /**
+     * @ngdoc method
+     * @name $scope#$getJobs
+     * @function
+     *
+     * @description Retrieves the jobs of an instance.
+     *
+     * @param {Object} p_instance The instance.
+     *
+     * @returns {Array.<Object>} An array containing the jobs.
+     */
     $scope.getJobs = function (p_instance) {
+        // console.debug("getJobs " + p_instance.name);
         return $scope.jobs[p_instance.name];
-    }
+    };
 
+    /**
+     * @ngdoc method
+     * @name $scope#$isLastBuildOk
+     * @function
+     *
+     * @description Determines if the last build of the job is OK (compile and test successful).
+     *
+     * @param {Object} p_job The job.
+     *
+     * @returns {boolean} true if the last build of the job is OK, false otherwise.
+     */
+    $scope.isLastBuildOk = function (p_job) {
+        // console.debug("isLastBuildOk " + p_job.name + " : " + angular.equals("blue", p_job.color));
+        return angular.equals("blue", p_job.color);
+    };
 });
